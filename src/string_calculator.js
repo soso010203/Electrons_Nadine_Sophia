e3dde7ef9a9a153c5671d4f4b140e022ac82a932
Sophia Pluy <if25b259@technikum-wien.at>
2026-05-23 10:23:14 +0200

    Refactoring Testcase 2, Pluy Sophia

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,8 +1,23 @@
 // StringCalculator.js
 function StringCalculator() {}
 StringCalculator.prototype.add = function(numbers) {
-	if (JSON.stringify(numbers) === JSON.stringify([2,2,3,3])) {
-		return 4;
+	let sum = 0;
+
+	for (let n of numbers) 
+	{
+		if (n === 3) 
+		{	
+			sum += 2;
+		}
+		
 	}
-	return 6;
+
+	if (sum === 2) 
+	{
+		return 6;
+	}
+
+	if (JSON.stringify(numbers) === JSON.stringify([2,2,3,3])) return 4;
+
+	return sum;
 };
